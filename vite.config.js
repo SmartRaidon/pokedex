79899770b4c9b9a67edcabfe9a830ebf724f85ab
Smarties <355510@via.dk>
2026-04-22 23:07:13 +0200

    vite config fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/smartraidon.github.io/",
+  base: "/pokedex/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
